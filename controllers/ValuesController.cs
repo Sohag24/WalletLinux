d316bcb416dd42c2007a6b8423928a0355ae8a1d
mdsohagali <mdsohagali@MSA-ST-LP>
2023-03-16 14:01:29 +0600

    AllFB_Added_KeyFileOmmited

--- controllers/ValuesController.cs
+++ controllers/ValuesController.cs
@@ -43,6 +43,15 @@ namespace WebApplication2.controllers
             return GetUTCDateTime2().ToString();
         }
 
+       
+        // POST api/<WalletController>
+        [HttpPost("CreateVault")]
+        public async Task<string> CreateVault([FromBody] JsonElement body)
+        {
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(EndPoints.VaultCreate, ApiMethods.Post, body);
+        }
+
         [HttpGet("GetVaults")]
         public async Task<string> GetVaults()
         {
@@ -50,6 +59,96 @@ namespace WebApplication2.controllers
             FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
             return await FG.CallApi(EndPoints.VaultAccounts, ApiMethods.Get, EmptyJson);
         }
+
+        // POST api/<WalletController>
+        [HttpPost("GetVaultsByVaultId")]
+        public async Task<string> GetVaultsByVaultId([FromBody] JsonElement body)
+        {
+            string BodyStr = System.Text.Json.JsonSerializer.Serialize(body);
+            dynamic data = JObject.Parse(BodyStr);
+            string endPoint = EndPoints.VaultCreate + "/" + data.vaultId;
+            string? eosAccountName = data.eosAccountName;
+
+            JsonElement EmptyJson = new JsonElement();
+
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(endPoint, ApiMethods.Get, EmptyJson);
+        }
+
+        // Get api/<WalletController>
+        [HttpGet("GetSupportedAssets")]
+        public async Task<string> GetSupportedAssets()
+        {
+            JsonElement EmptyJson = new JsonElement();
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(EndPoints.SupportedAssets, ApiMethods.Get, EmptyJson);
+        }
+
+        // POST api/<WalletController>
+        [HttpPost("AddAssets")]
+        public async Task<string> AddAssets([FromBody] JsonElement body)
+        {
+            string BodyStr = System.Text.Json.JsonSerializer.Serialize(body);
+            dynamic data = JObject.Parse(BodyStr);
+            string endPoint = EndPoints.VaultCreate + "/" + data.vaultId + "/" + data.assetId;
+            string? eosAccountName = data.eosAccountName;
+
+            JsonElement newBody = new JsonElement();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (Utf8JsonWriter writer = new Utf8JsonWriter(stream))
+                {
+                    writer.WriteStartObject();
+
+                    writer.WriteString("eosAccountName", eosAccountName);
+
+                    writer.WriteEndObject();
+                }
+
+                byte[] json = stream.ToArray();
+                JsonDocument document = JsonDocument.Parse(json);
+                newBody = document.RootElement;
+            }
+
+
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(endPoint, ApiMethods.Post, newBody);
+        }
+
+
+        [HttpPost("HideVault")]
+        public async Task<string> HideVault([FromBody] JsonElement body)
+        {
+            string BodyStr = System.Text.Json.JsonSerializer.Serialize(body);
+            dynamic data = JObject.Parse(BodyStr);
+            string endPoint = EndPoints.VaultCreate + "/" + data.vaultAccountId + "/hide";
+
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(endPoint, ApiMethods.Post, body);
+        }
+
+        [HttpPost("UnhideVault")]
+        public async Task<string> UnhideVault([FromBody] JsonElement body)
+        {
+            string BodyStr = System.Text.Json.JsonSerializer.Serialize(body);
+            dynamic data = JObject.Parse(BodyStr);
+            string endPoint = EndPoints.VaultCreate + "/" + data.vaultAccountId + "/unhide";
+
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(endPoint, ApiMethods.Post, body);
+        }
+
+        [HttpPost("Activate")]
+        public async Task<string> Activate([FromBody] JsonElement body)
+        {
+            string BodyStr = System.Text.Json.JsonSerializer.Serialize(body);
+            dynamic data = JObject.Parse(BodyStr);
+            string endPoint = EndPoints.VaultCreate + "/" + data.vaultAccountId + "/" + data.assetId + "/activate";
+
+            FireBlocks_GateWay FG = new FireBlocks_GateWay(_configuration);
+            return await FG.CallApi(endPoint, ApiMethods.Post, body);
+        }
+
 
         public static DateTime GetUTCDateTime()
         {
