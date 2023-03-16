817354cd71f5c3ef44b58e3a3b0c351445fcbe17
mdsohagali <mdsohagali@MSA-ST-LP>
2023-03-16 21:08:17 +0600

    CorsAdded

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-var app = builder.Build();
 
+builder.Services.AddCors(policyBuilder =>
+    policyBuilder.AddDefaultPolicy(policy =>
+        policy.WithOrigins("*").AllowAnyHeader().AllowAnyHeader())
+);
+
+var app = builder.Build();
+app.UseCors();
 app.MapGet("/", () => "{'msg':'hellow there','status':200}");
 app.MapControllers();
 app.Run();
